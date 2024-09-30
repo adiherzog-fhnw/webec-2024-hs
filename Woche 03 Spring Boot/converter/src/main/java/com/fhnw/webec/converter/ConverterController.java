346c67c2369a7be60b1b38954b1334accaa7a804
Adrian Herzog <adrian.herzog@gmail.com>
2024-09-30 23:03:30 +0200

    Rename

--- Woche 03 Spring Boot/converter/src/main/java/com/fhnw/webec/converter/ConverterController.java
+++ Woche 03 Spring Boot/converter/src/main/java/com/fhnw/webec/converter/ConverterController.java
@@ -12,11 +12,11 @@ public class ConverterController {
 
         if (inches != null) {
             var cm = inches * 2.54;
-            int cmValue = (int) Math.floor(cm);
-            int mmValue = (int) Math.round((cm - cmValue) * 10);
+            int cmOnly = (int) Math.floor(cm);
+            int mm = (int) Math.round((cm - cmOnly) * 10);
 
-            model.addAttribute("cm", cmValue);
-            model.addAttribute("mm", mmValue);
+            model.addAttribute("cm", cmOnly);
+            model.addAttribute("mm", mm);
             model.addAttribute("inches", inches);
         }
 

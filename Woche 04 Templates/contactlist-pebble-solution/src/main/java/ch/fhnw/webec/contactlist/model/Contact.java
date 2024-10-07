4e08353017fe472f0ace4c60ea7e1f117d5f0132
Adrian Herzog <adrian.herzog@gmail.com>
2024-10-07 12:41:40 +0200

    Solution template

---
+++ Woche 04 Templates/contactlist-pebble-solution/src/main/java/ch/fhnw/webec/contactlist/model/Contact.java
@@ -0,0 +1,70 @@
+package ch.fhnw.webec.contactlist.model;
+
+import java.util.List;
+
+public class Contact {
+
+    private int id;
+    private String firstName;
+    private String lastName;
+    private List<String> email;
+    private List<String> phone;
+    private String jobTitle;
+    private String company;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public List<String> getEmail() {
+        return email;
+    }
+
+    public void setEmail(List<String> email) {
+        this.email = email;
+    }
+
+    public List<String> getPhone() {
+        return phone;
+    }
+
+    public void setPhone(List<String> phone) {
+        this.phone = phone;
+    }
+
+    public String getJobTitle() {
+        return jobTitle;
+    }
+
+    public void setJobTitle(String jobTitle) {
+        this.jobTitle = jobTitle;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+}

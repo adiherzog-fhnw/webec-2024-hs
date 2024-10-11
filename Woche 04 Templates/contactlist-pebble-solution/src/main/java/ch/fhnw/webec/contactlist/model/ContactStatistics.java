d1958475c4b13c6859422d18892e422a44f68713
Adrian Herzog <adrian.herzog@gmail.com>
2024-10-12 00:06:06 +0200

    Musterlösung Woche 4

---
+++ Woche 04 Templates/contactlist-pebble-solution/src/main/java/ch/fhnw/webec/contactlist/model/ContactStatistics.java
@@ -0,0 +1,3 @@
+package ch.fhnw.webec.contactlist.model;
+
+public record ContactStatistics(int contactCount, int phoneNumberCount, int emailAddressCount) {}
---
+++ Woche 04 Templates/contactlist-pebble-solution/src/test/java/ch/fhnw/webec/contactlist/ContactListApplicationTests.java
@@ -0,0 +1,12 @@
+package ch.fhnw.webec.contactlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactListApplicationTests {
+
+    @Test
+    void contextLoads() {}
+
+}
